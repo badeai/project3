704df32b416bbae9a6986be9012367347ed55811
badeai <90259268+badeai@users.noreply.github.com>
2022-06-16 19:33:54 +0300

    replaced event with subject

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,11 @@
-import { Subject } from 'rxjs';
 import { ShoppingListService } from './../shopping-list/shopping-list.service';
 import { Injectable } from '@angular/core';
-import { inject } from '@angular/core/testing';
+
 import { Ingredient } from '../shared/ingredients.model';
 import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new Subject<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
       'A test recipe',
